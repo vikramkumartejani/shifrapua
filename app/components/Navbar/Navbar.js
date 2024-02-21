128c7e7ab86e57a44b15c479870cade7fd332a04
DileepAmrani <amranidileep44@gmail.com>
2024-02-21 08:11:03 +0500

    'updatenavbar'

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,12 +1,18 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import "./navbar.css";
- import { TiHeartFullOutline } from "react-icons/ti";
+import { TiHeartFullOutline } from "react-icons/ti";
 import { IoMenu } from "react-icons/io5";
 import Image from "next/image";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -22,28 +28,72 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className={`nav-container ${isScrolled ? 'scrolled' : ''}`}>
+    <div className={`nav-container ${isScrolled ? "scrolled" : ""}`}>
       <div className="menu-container">
         <a href="/donation" className="donation-link">
           <TiHeartFullOutline className="heart-icon" />
           לתרומה
         </a>
-        <h1 className="mobile-menu"><IoMenu className="menu-icon" /></h1>
+        <button onClick={handleToggleMenu} className="mobile-menu">
+          <IoMenu className="menu-icon" />
+        </button>
       </div>
       <div className="links-container">
         <ul className="links">
-          <li><a href="/contact">צרו קשר</a></li>
-          <li><a href="/confidentiality">חסויות</a></li>
-          <li><a href="/thanks">תודות</a></li>
-          <li><a href="/servicesmother">השירות ליולדת</a></li>
-          <li><a href="/about">אודותינו</a></li>
-          <li><a href="/">דף הבית</a></li>
+          <li>
+            <a href="/contact">צרו קשר</a>
+          </li>
+          <li>
+            <a href="/confidentiality">חסויות</a>
+          </li>
+          <li>
+            <a href="/thanks">תודות</a>
+          </li>
+          <li>
+            <a href="/servicesmother">השירות ליולדת</a>
+          </li>
+          <li>
+            <a href="/about">אודותינו</a>
+          </li>
+          <li>
+            <a href="/">דף הבית</a>
+          </li>
         </ul>
         <div className="">
-        <a href="/">
-          <Image src="/assets/Logo.png" width={150} height={160} alt="Logo" className="logo-image"/>
-        </a>
+          <a href="/">
+            <Image
+              src="/assets/Logo.png"
+              width={150}
+              height={160}
+              alt="Logo"
+              className="logo-image"
+            />
+          </a>
         </div>
+        {isMenuOpen && (
+          <div className="menu-toggle ">
+          <ul className="links-menu">
+          <li>
+            <a href="/contact">צרו קשר</a>
+          </li>
+          <li>
+            <a href="/confidentiality">חסויות</a>
+          </li>
+          <li>
+            <a href="/thanks">תודות</a>
+          </li>
+          <li>
+            <a href="/servicesmother">השירות ליולדת</a>
+          </li>
+          <li>
+            <a href="/about">אודותינו</a>
+          </li>
+          <li>
+            <a href="/">דף הבית</a>
+          </li>
+        </ul>
+          </div>
+        )}
       </div>
     </div>
   );
