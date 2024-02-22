d44eb03c1ff936556f8c01e339f0975904ef5176
DileepAmrani <amranidileep44@gmail.com>
2024-02-22 13:21:28 +0500

    'update'

--- app/components/ImagesGallery/ImagesGallery.js
+++ app/components/ImagesGallery/ImagesGallery.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './imagesgallery.css'
 
 const images = [
-  { src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-1.jpg', alt: 'gallery1s1-1' },
-  { src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/nijlsdbv.jpg', alt: 'nijlsdbv' },
-  { src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-12.jpg', alt: 'gallery1s1-12' },
-  { src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-9.jpg', alt: 'gallery1s1-9' },
-  { src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/IMG-20180110-WA0021.jpg', alt: 'IMG-20180110-WA0021' },
-  { src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-3.jpg', alt: 'gallery1s1-3' },
-  { src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-14.jpg', alt: 'gallery1s1-14' },
-  { src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-10.jpg', alt: 'gallery1s1-10' },
-  { src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-6.jpg', alt: 'gallery1s1-6' }
-];
+  { id: 1, src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-1.jpg', alt: 'gallery1s1-1' },
+  { id: 2, src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/nijlsdbv.jpg', alt: 'nijlsdbv' },
+  { id: 3, src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-12.jpg', alt: 'gallery1s1-12' },
+  { id: 4, src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-9.jpg', alt: 'gallery1s1-9' },
+  { id: 5, src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/IMG-20180110-WA0021.jpg', alt: 'IMG-20180110-WA0021' },
+  { id: 6, src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-3.jpg', alt: 'gallery1s1-3' },
+  { id: 7, src: 'https://shifrapua.org.il/wp-content/uploads/2019/01/gallery1s1-14.jpg', alt: 'gallery1s1-14' },
+  ];
 
 const ImagesGallery = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openPopup = (id) => {
+    setSelectedImage(id);
+  };
+
+  const closePopup = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div className="gallery">
-      {images.map((image, index) => (
-        <div key={index} className="item">
+      {images.map((image) => (
+        <div key={image.id} className="item" onClick={() => openPopup(image.id)}>
           <img src={image.src} alt={image.alt} />
         </div>
       ))}
+      {selectedImage && (
+        <div className="popup" onClick={closePopup}>
+          <div className="popup-inner">
+            <img src={images.find(img => img.id === selectedImage).src} alt={images.find(img => img.id === selectedImage).alt} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
+
 
 export default ImagesGallery;
